b50be88adf3e9fdf853fcc69939333ae7b6b34bb
yunji0387 <yunji0387@gmail.com>
2024-12-31 17:41:16 +0800

    Add last updated timestamp and enhance loading button in StatsCard component

--- app/components/StatsCard.tsx
+++ app/components/StatsCard.tsx
@@ -13,6 +13,7 @@ const StatsCard: React.FC = () => {
   const [ethereumPrice, setEthereumPrice] = useState<AssetPriceData | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [lastUpdated, setLastUpdated] = useState<string | null>(null);
 
   const fetchAssetPrice = async () => {
     if (!process.env.NEXT_PUBLIC_ASSET_DATA_API_URL) {
@@ -34,6 +35,9 @@ const StatsCard: React.FC = () => {
       setSilverPrice(assetData.XAG);
       setBitcoinPrice(assetData.BTC);
       setEthereumPrice(assetData.ETH);
+
+      const currentTime = new Date();
+      setLastUpdated(currentTime.toLocaleString());
     } catch (err: any) {
       setError(err.message || "An unexpected error occurred");
     } finally {
@@ -50,9 +54,31 @@ const StatsCard: React.FC = () => {
       <div className="flex flex-row gap-2 items-center">
         <button
           onClick={fetchAssetPrice}
-          className={`text-white transition duration-300 ease-in-out transform ${
-            !loading && "hover:scale-105 hover:rotate-90"
-          }`}
+          className={`flex items-center justify-center text-white transition duration-300 ease-in-out transform ${!loading && "hover:rotate-90"
+            }`}
+          disabled={loading}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            width="24"
+            height="24"
+            className={`${loading && "animate-spin"}`}
+          >
+            <polyline points="20.5 4 20.5 8 16.5 8"></polyline>
+            <path d="M21 12A9 9 0 1 1 19 6.3"></path>
+          </svg>
+        </button>
+
+        {/* <button
+          onClick={fetchAssetPrice}
+          className={`text-white transition duration-300 ease-in-out transform ${!loading && "hover:scale-105 hover:rotate-90"
+            }`}
           disabled={loading}
         >
           <svg
@@ -70,11 +96,15 @@ const StatsCard: React.FC = () => {
             <polyline points="19 2 19 6 15 6"></polyline>
             <path d="M20.49 12A9 9 0 1 1 19 5.51"></path>
           </svg>
-        </button>
+        </button> */}
         <h2 className="text-xl font-bold">Price</h2>
       </div>
-      {/* {loading && <p>Loading...</p>} */}
       {error && <p className="text-red-500">Error: {error}</p>}
+      {lastUpdated && (
+        <p className="text-gray-500 text-sm">
+          Last Updated: {lastUpdated}
+        </p>
+      )}
       {goldPrice && (
         <div>
           <p>Gold: {goldPrice.price}</p>
